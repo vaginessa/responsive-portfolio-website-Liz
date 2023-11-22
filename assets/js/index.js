847e00488884dc2403e5fc13b0d08b2ad102a121
Agus <agustinriveratapi3@gmail.com>
2023-11-22 16:05:30 -0300

    firstcommit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,22 @@
 /*=============== SERVICES MODAL ===============*/
+const modal = document.querySelectorAll('.services__modal'),
+      modalButton = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
 
+let activeModal = (modalClick) => {
+    modal[modalClick].classList.add('active-modal')
+}
 
-/*=============== SWIPER TESTIMONIAL ===============*/
+modalButton.forEach((modalButton, i) => {
+    modalButton.addEventListener('click', ()=>{
+        activeModal(i)
+    })
+})
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener('click', ()=>{
+        modal.forEach((modalRemove) => {
+            modalRemove.classList.remove('active-modal')
+        })
+    })
+})
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,95 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SWIPER SHOE ===============*/
+let swiperShoes = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: 'creative',
+    creativeEffect:{
+        prev:{
+            translate: [-100, 0, -500],
+            opacity: 0,
+        },
+        next:{
+            translate: [100, 0, -500],
+            opacity: 0,
+        },
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
